6fb3f0f2e41203dcf6b4e0c5233a339b5bf12863
KProvance <kevin.provance@gmail.com>
2017-10-03 00:10:57 -0400

    no @ symbol in our ads id, please

--- ReduxCore/inc/classes/class.redux_args.php
+++ ReduxCore/inc/classes/class.redux_args.php
@@ -116,8 +116,8 @@ if (!class_exists('Redux_Args')) {
 
             $args = $this->filters($args);
             
-            $this->parent->core_instance    = chr(64 + rand( 0, 26 )) . time() . '_' . rand(0,1000000);
-            $this->parent->core_thread      = chr(64 + rand( 0, 26 )) . time() . '_' . rand(0,1000000);
+            $this->parent->core_instance    = chr(64 + rand( 1, 26 )) . time() . '_' . rand(0,1000000);
+            $this->parent->core_thread      = chr(64 + rand( 1, 26 )) . time() . '_' . rand(0,1000000);
             
             if ( $args['opt_name'] == $this->parent->old_opt_name ) {
                 $this->parent->old_opt_name = null;
