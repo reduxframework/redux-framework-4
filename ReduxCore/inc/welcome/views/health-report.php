cd6be51f96b0d9d24d6a9fb69829d34d094b8242
Tofandel <adrien.foulon.lfgeb@gmail.com>
2019-02-27 13:39:15 +0100

    $callers not $_callers

--- ReduxCore/inc/welcome/views/health-report.php
+++ ReduxCore/inc/welcome/views/health-report.php
@@ -761,7 +761,7 @@ $sysinfo = Redux_Helpers::compile_system_status( false, true );
 					?>
 					</td>
 				</tr>
-				<?php if ( ! empty( Redux_Core::$_callers[ $inst ] ) ) { ?>
+				<?php if ( ! empty( Redux_Core::$callers[ $inst ] ) ) { ?>
 					<tr>
 						<td data-export-label="output_tag">Calling Files</td>
 						<td class="help">
