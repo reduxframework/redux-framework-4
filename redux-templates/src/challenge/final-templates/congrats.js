f17fe7353de4becad00e71b8f2e3b38018e06870
Dovy Paukstys <me@dovy.io>
2020-05-22 07:43:28 -0400

    Small string fixes.

--- redux-templates/src/challenge/final-templates/congrats.js
+++ redux-templates/src/challenge/final-templates/congrats.js
@@ -29,7 +29,7 @@ function ChallengeCongrats(props) {
     return (
         <div className="redux-templates-modal-overlay">
             <div className="redux-templates-modal-wrapper challenge-popup-wrapper">
-                <div class="challenge-popup-header challenge-popup-header-congrats" 
+                <div class="challenge-popup-header challenge-popup-header-congrats"
                     style={{backgroundImage: `url(${redux_templates.plugin + 'assets/img/popup-congrats.png'})`}}>
                     <a className="challenge-popup-close" onClick={closeModal}>
                         <i class="fa fa-times-circle fa-lg"></i>
@@ -38,13 +38,13 @@ function ChallengeCongrats(props) {
                 <div class="challenge-popup-content">
                     <h3>{__( 'Congrats, you did it!', redux_templates.i18n )}</h3>
                     <p>
-                        You completed the ReduxTemplates Challenge in <b>{helper.getLocalizedDuration()}</b>. 
-                        Share your success story with other ReduxTemplates users and help us spread the word 
-                        <b>by giving ReduxTemplates a 5-star rating ({ratingStars}) on WordPress.org</b>. 
+                        You completed the Redux Challenge in <b>{helper.getLocalizedDuration()}</b>.
+                        Share your success story with other Redux users and help us spread the word
+                        <b>by giving ReduxTemplates a 5-star rating ({ratingStars}) on WordPress.org</b>.
                         Thanks for your support and we look forward to bringing more awesome features.
                     </p>
-                    <a href="https://wordpress.org/support/plugin/redux-templates/reviews/?filter=5#new-post" class="challenge-popup-btn challenge-popup-rate-btn" target="_blank" rel="noopener">
-                        {__( 'Rate ReduxTemplates on Wordpress.org', redux_templates.i18n ) }
+                    <a href="https://wordpress.org/support/plugin/redux-framework/reviews/?filter=5#new-post" class="challenge-popup-btn challenge-popup-rate-btn" target="_blank" rel="noopener">
+                        {__( 'Rate Redux on Wordpress.org', redux_templates.i18n ) }
                         <span class="dashicons dashicons-external"></span>
                     </a>
                 </div>
