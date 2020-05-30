0d94b9c4f2a6853436647fbe1e72a51fbdb0e339
Kevin Provance <kevin.provance@gmail.com>
2020-05-29 21:57:05 -0400

    Sample Pro fields, Reorganize sample config

---
+++ sample/sections/pro-fields/metaboxes.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * Redux Framework Sample Metabox Config File
+ * For full documentation, please visit: http://docs.reduxframework.com/
+ * Metabox Lite support the following fields only:  checkbox, radio, text, textarea, media, & color
+ * Post Format and Post Template options are not avaialble in Metabox Lite.
+ * These advanced options are available in Redux Pro.
+ *
+ * @package Redux Framework
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'Redux_Metaboxes' ) ) {
+	return;
+}
+
+Redux_Metaboxes::set_box(
+	$opt_name,
+	array(
+		'id'         => 'opt-metaboxes',
+		'title'      => esc_html__( 'Metabox Options', 'your-domain-here' ),
+		'post_types' => array( 'page', 'post' ),
+		'position'   => 'normal', // normal, advanced, side.
+		'priority'   => 'high', // high, core, default, low.
+		'sections'   => array(
+			array(
+				'title'  => esc_html__( 'Basic Fields', 'your-domain-here' ),
+				'id'     => 'opt-basic-fields',
+				'desc'   => esc_html__( 'Redux Framework was created with the developer in mind. It allows for any theme developer to have an advanced theme panel with most of the features a developer would need. For more information check out the Github repo at:', 'your-domain-here' ) . '  <a href="https://github.com/ReduxFramework/Redux-Framework">https://github.com/ReduxFramework/Redux-Framework</a>',
+				'icon'   => 'el-icon-cogs',
+				'fields' => array(
+					array(
+						'id'       => 'opt-checkbox',
+						'type'     => 'checkbox',
+						'title'    => esc_html__( 'Checkbox', 'your-domain-here' ),
+						'subtitle' => esc_html__( 'Basic Checkbox field.', 'your-domain-here' ),
+						'default'  => true,
+					),
+					array(
+						'id'       => 'opt-radio',
+						'type'     => 'radio',
+						'title'    => esc_html__( 'Radio Button', 'your-domain-here' ),
+						'subtitle' => esc_html__( 'Basic Radio Button field.', 'your-domain-here' ),
+						'options'  => array(
+							'1' => esc_html__( 'Option 1', 'your-domain-here' ),
+							'2' => esc_html__( 'Option 2', 'your-domain-here' ),
+							'3' => esc_html__( 'Option 3', 'your-domain-here' ),
+						),
+						'default'  => '2',
+					),
+					array(
+						'id'       => 'opt-media',
+						'type'     => 'media',
+						'url'      => true,
+						'title'    => esc_html__( 'Media w/ URL', 'your-domain-here' ),
+						'compiler' => 'true',
+						'desc'     => esc_html__( 'Basic media uploader with disabled URL input field.', 'your-domain-here' ),
+						'subtitle' => esc_html__( 'Upload any media using the WordPress native uploader', 'your-domain-here' ),
+						'default'  => array( 'url' => 'http://s.wordpress.org/style/images/codeispoetry.png' ),
+					),
+					array(
+						'id'       => 'gallery',
+						'type'     => 'gallery',
+						'title'    => esc_html__( 'Add/Edit Gallery', 'so-panels' ),
+						'subtitle' => esc_html__( 'Create a new Gallery by selecting existing or uploading new images using the WordPress native uploader', 'so-panels' ),
+						'desc'     => esc_html__( 'This is the description field, again good for additional info.', 'your-domain-here' ),
+					),
+					array(
+						'id'      => 'opt-slider',
+						'type'    => 'slider',
+						'title'   => esc_html__( 'JQuery UI Slider Example 2 w/ Steps (5)', 'your-domain-here' ),
+						'desc'    => esc_html__( 'JQuery UI slider description. Min: 0, max: 300, step: 5, default value: 75', 'your-domain-here' ),
+						'default' => '0',
+						'min'     => '0',
+						'step'    => '5',
+						'max'     => '300',
+					),
+					array(
+						'id'      => 'opt-spinner',
+						'type'    => 'spinner',
+						'title'   => esc_html__( 'JQuery UI Spinner Example 1', 'your-domain-here' ),
+						'desc'    => esc_html__( 'JQuery UI spinner description. Min:20, max: 100, step:20, default value: 40', 'your-domain-here' ),
+						'default' => '40',
+						'min'     => '20',
+						'step'    => '20',
+						'max'     => '100',
+					),
+
+					array(
+						'id'       => 'switch-on',
+						'type'     => 'switch',
+						'title'    => esc_html__( 'Switch On', 'your-domain-here' ),
+						'subtitle' => esc_html__( 'Look, it\'s on!', 'your-domain-here' ),
+						'default'  => 1,
+					),
+				),
+			),
+
+			array(
+				'title'      => esc_html__( 'Text Fields', 'your-domain-here' ),
+				'desc'       => esc_html__( 'Redux Framework was created with the developer in mind. It allows for any theme developer to have an advanced theme panel with most of the features a developer would need. For more information check out the Github repo at:', 'your-domain-here' ) . '  <a href="https://github.com/ReduxFramework/Redux-Framework">https://github.com/ReduxFramework/Redux-Framework</a>',
+				'icon'       => 'el-icon-cog',
+				'id'         => 'opt-text-fields',
+				'subsection' => true,
+				'fields'     => array(
+					array(
+						'title' => esc_html__( 'Text Field', 'your-domain-here' ),
+						'id'    => 'opt-text',
+						'type'  => 'text',
+					),
+					array(
+						'title' => esc_html__( 'Textarea Field', 'your-domain-here' ),
+						'id'    => 'opt-textarea',
+						'type'  => 'textarea',
+					),
+				),
+			),
+
+			array(
+				'title'  => esc_html__( 'Color Field', 'your-domain-here' ),
+				'desc'   => esc_html__( 'Redux Framework was created with the developer in mind. It allows for any theme developer to have an advanced theme panel with most of the features a developer would need. For more information check out the Github repo at:', 'your-domain-here' ) . '  <a href="https://github.com/ReduxFramework/Redux-Framework">https://github.com/ReduxFramework/Redux-Framework</a>',
+				'icon'   => 'el-icon-pencil',
+				'id'     => 'color-section',
+				'fields' => array(
+					array(
+						'id'       => 'opt-color',
+						'type'     => 'color',
+						'title'    => __( 'Color Field', 'your-domain-here' ),
+						'default'  => '#333333',
+						'required' => array( 'opt-layout', '=', 'on' ),
+					),
+				),
+			),
+			array(
+				'title'  => esc_html__( 'Layout', 'your-domain-here' ),
+				'desc'   => esc_html__( 'Redux Framework was created with the developer in mind. It allows for any theme developer to have an advanced theme panel with most of the features a developer would need. For more information check out the Github repo at:', 'your-domain-here' ) . '  <a href="https://github.com/ReduxFramework/Redux-Framework">https://github.com/ReduxFramework/Redux-Framework</a>',
+				'icon'   => 'el-icon-pencil',
+				'id'     => 'home-layout',
+				'fields' => array(
+					array(
+						'id'       => 'homepage_blocks',
+						'type'     => 'sorter',
+						'title'    => 'Homepage Layout Manager',
+						'desc'     => 'Organize how you want the layout to appear on the homepage',
+						'compiler' => 'true',
+						'required' => array( 'layout', '=', '1' ),
+						'options'  => array(
+							'enabled'  => array(
+								'placebo'    => 'placebo',
+								'highlights' => 'Highlights',
+								'slider'     => 'Slider',
+								'staticpage' => 'Static Page',
+								'services'   => 'Services',
+							),
+							'disabled' => array(
+								'placebo' => 'placebo',
+							),
+						),
+					),
+					array(
+						'id'       => 'slides',
+						'type'     => 'slides',
+						'title'    => esc_html__( 'Slides Options', 'your-domain-here' ),
+						'subtitle' => esc_html__( 'Unlimited slides with drag and drop sortings.', 'your-domain-here' ),
+						'desc'     => esc_html__( 'This field will store all slides values into a multidimensional array to use into a foreach loop.', 'your-domain-here' ),
+					),
+				),
+			),
+		),
+	),
+);
+
+Redux_Metaboxes::set_box(
+	$opt_name,
+	array(
+		'id'         => 'opt-metaboxes-2',
+		'post_types' => array( 'page', 'post' ),
+		'position'   => 'side', // normal, advanced, side.
+		'priority'   => 'high', // high, core, default, low.
+		'sections'   => array(
+			array(
+				'icon_class' => 'icon-large',
+				'icon'       => 'el-icon-home',
+				'fields'     => array(
+					array(
+						'title'   => esc_html__( 'Cross Box Required', 'your-domain-here' ),
+						'desc'    => esc_html__( 'Required arguments work across metaboxes! Click on Color Field under Metabox Options then adjust this field to see the fields within show or hide.', 'your-domain-here' ),
+						'id'      => 'opt-layout',
+						'type'    => 'radio',
+						'options' => array(
+							'on'  => esc_html__( 'On', 'your-domain-here' ),
+							'off' => esc_html__( 'Off', 'your-domain-here' ),
+						),
+						'default' => 'on',
+					),
+				),
+			),
+		),
+	),
+);
+
+Redux_Metaboxes::set_box(
+	$opt_name,
+	array(
+		'id'         => 'opt-metaboxes-3',
+		'post_types' => array( 'page', 'post' ),
+		'position'   => 'side', // normal, advanced, side.
+		'priority'   => 'high', // high, core, default, low.
+		'sections'   => array(
+			array(
+				'icon_class' => 'icon-large',
+				'icon'       => 'el-icon-home',
+				'fields'     => array(
+					array(
+						'id'      => 'sidebar',
+						'title'   => esc_html__( 'Sidebar', 'your-domain-here' ),
+						'desc'    => esc_html__( 'Please select the sidebar you would like to display on this page. Note: You must first create the sidebar under Appearance > Widgets.', 'your-domain-here' ),
+						'type'    => 'select',
+						'data'    => 'sidebars',
+						'default' => 'None',
+					),
+				),
+			),
+		),
+	),
+);
---
+++ sample/sections/pro-fields/typography.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Redux Pro Typography Sample config.
+ *
+ * For full documentation, please visit: http://docs.reduxframework.com/
+ *
+ * @package Redux Pro
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+Redux::setSection(
+	$opt_name,
+	array(
+		'title'  => esc_html__( 'Typography', 'your-domain-here' ),
+		'id'     => 'pro-typography',
+		'desc'   => esc_html__( 'For full documentation on this field, visit: ', 'your-domain-here' ) . '<a href="//docs.reduxframework.com/core/fields/typography/" target="_blank">docs.reduxframework.com/core/fields/typography/</a>',
+		'subsection' => true,
+		'fields' => array(
+			array(
+				'id'                => 'opt-pro-typography-body',
+				'type'              => 'typography',
+				'title'             => esc_html__( 'Body Font', 'your-domain-here' ),
+				'subtitle'          => esc_html__( 'Specify the body font properties.', 'your-domain-here' ),
+				'google'            => true,
+				'font_family_clear' => false,
+				'text-shadow'       => true,
+				'color_alpha'       => true,
+				'margin-top'        => true,
+				'margin-bottom'     => true,
+				'default'           => array(
+					'color'         => '#dd9933',
+					'font-size'     => '30px',
+					'font-family'   => 'Arial, Helvetica, sans-serif',
+					'font-weight'   => 'Normal',
+					'margin-top'    => '2px',
+					'margin-bottom' => '2px',
+				),
+				'output'            => array( '.site-description, .entry-title' ),
+			),
+		),
+	)
+);
