c51a2b9429603ea114b78569278d48e4c8c9cb28
Dovy Paukstys <me@dovy.io>
2020-06-15 14:09:41 -0400

    More fixes

--- redux-templates/src/modal-import-wizard/ReduxTemplatesPremiumBox.js
+++ redux-templates/src/modal-import-wizard/ReduxTemplatesPremiumBox.js
@@ -4,18 +4,17 @@ export default function ReduxTemplatesPremiumBox(props) {
     return (
         <div className="redux-templates-modal-body">
             <div class="section-box premium-box">
-                <h3>{__('Redux Premium is Required', redux_templates.i18n)}</h3>
+                <h3>{__('Upgrade to Redux Pro', redux_templates.i18n)}</h3>
 
-                <p>{__('Upgrade now to use this template and enjoy these benefits:', redux_templates.i18n)}</p>
+                <p>{__('Upgrade to unlock 1,000+ designs to build your pages quick!', redux_templates.i18n)}</p>
                 <ul>
-                    <li><strong>{__('Endless', redux_templates.i18n)}</strong> {__('Updates to the Library', redux_templates.i18n)}</li>
-                    <li><strong>{__('Unlimited', redux_templates.i18n)}</strong> {__('Use of the Library', redux_templates.i18n)}</li>
-                    <li><strong>500+</strong> {__('Block', redux_templates.i18n)}</li>
-                    <li><strong>125+</strong> {__('Page', redux_templates.i18n)}</li>
-                    <li><strong>20+</strong> {__('Template Kits', redux_templates.i18n)}</li>
+                    <li><strong>{__('Unlimited', redux_templates.i18n)}</strong> {__('access to the Library', redux_templates.i18n)}</li>
+                    <li><strong>{__('Google Fonts', redux_templates.i18n)}</strong> {__('always up to date.', redux_templates.i18n)}</li>
+                    <li><strong>{__('Advanced Customizer', redux_templates.i18n)}</strong> {__('for settings.', redux_templates.i18n)}</li>
+                    <li><strong>{__('And so much more!', redux_templates.i18n)}</strong></li>
                 </ul>
                 <p>
-                    <a href={redux_templates.u} class="redux-templates-upgrade-button" title="Redux Premium"
+                    <a href={redux_templates.u} class="redux-templates-upgrade-button" title="{__('Redux Pro', redux_templates.i18n)}"
                        target='_blank'>{__('Upgrade Now', redux_templates.i18n)}</a>
                 </p>
             </div>
