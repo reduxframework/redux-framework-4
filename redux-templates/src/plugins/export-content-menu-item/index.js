df79363fc74b2f2bf2bbccff6df0012651f85409
Alfred Takala <alfredtakala@yandex.com>
2020-06-28 01:57:37 +0800

    #108 full page export working

--- redux-templates/src/plugins/export-content-menu-item/index.js
+++ redux-templates/src/plugins/export-content-menu-item/index.js
@@ -1,61 +1,67 @@
 /**
  * WordPress dependencies
  */
-import { MenuItem } from '@wordpress/components';
 import { withDispatch, withSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { useCopyOnClick, compose, ifCondition } from '@wordpress/compose';
-import { useRef, useEffect } from '@wordpress/element';
+import { compose, ifCondition } from '@wordpress/compose';
+import { useRef } from '@wordpress/element';
+import { download } from '../export/file';
+const { registerPlugin } = wp.plugins;
+const { Fragment } = wp.element;
+const { PluginMoreMenuItem } = wp.editPost;
+
+const redux_templatesIcon = <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 19 19">
+  <g>
+    <path d="M10.9,17.7H7.4l-0.9-1.5l2.1-2.4L10.9,17.7L10.9,17.7z M5.6,16.1l-1.5,1.6H0.1L4,13.3L5.6,16.1L5.6,16.1z"/>
+    <polygon points="6.1,15.6 0.4,5.9 3.9,5.9 6.6,10.4 14.6,1.3 18.9,1.3 6.1,15.6 	"/>
+    </g>
+  </svg>;
 
 function ExportContentMenuItem( { createNotice, editedPostContent } ) {
-	const ref = useRef();
-	const hasCopied = useCopyOnClick( ref, editedPostContent );
+  const ref = useRef();
+  const exportFullpage = () => {
+		const fileContent = JSON.stringify( {
+			__file: 'core_block',
+			content: editedPostContent,
+		}, null, 2 );
 
-	useEffect( () => {
-		if ( ! hasCopied ) {
-			return;
-		}
+		const fileName = 'redux-template.json';
+		download( fileName, fileContent, 'application/json' );
+  }
 
-		createNotice( 'info', __( 'Export file created.' ), {
-			isDismissible: true,
-			type: 'snackbar',
-		} );
-	}, [ hasCopied ] );
 
-	return (
-		<MenuItem ref={ ref }>
-			{ hasCopied ? __( 'Copied!' ) : __( 'Export all to JSON' ) }
-		</MenuItem>
-	);
+  return (
+    <Fragment>
+      <PluginMoreMenuItem
+        icon={ redux_templatesIcon }
+        role="menuitemcheckbox"
+        onClick={ exportFullpage }
+      >
+        { __( 'Export full page', redux_templates.i18n ) }
+      </PluginMoreMenuItem>
+    </Fragment>
+  );
 }
 
-export default compose(
-	withSelect( ( select ) => ( {
-		editedPostContent: select( 'core/editor' ).getEditedPostAttribute(
-			'content'
-		),
-	} ) ),
-	withDispatch( ( dispatch ) => {
-		const { createNotice } = dispatch( 'core/notices' );
+const ExportContentMenu = compose(
+  withSelect( ( select ) => ( {
+    editedPostContent: select( 'core/editor' ).getEditedPostAttribute(
+      'content'
+    ),
+  } ) ),
+  withDispatch( ( dispatch ) => {
+    const { createNotice } = dispatch( 'core/notices' );
 
-		return {
-			createNotice,
-		};
-	} ),
-	ifCondition( ( { editedPostContent } ) => editedPostContent.length > 0 )
+    return {
+      createNotice,
+    };
+  } ),
+  ifCondition( ( { editedPostContent } ) => editedPostContent.length > 0 )
 )( ExportContentMenuItem );
 
 if (wp.plugins) {
-	// TODO - Get this working.
-	// const redux_templatesIcon = <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 19 19">
-	// 	<g>
-	// 		<path d="M10.9,17.7H7.4l-0.9-1.5l2.1-2.4L10.9,17.7L10.9,17.7z M5.6,16.1l-1.5,1.6H0.1L4,13.3L5.6,16.1L5.6,16.1z"/>
-	// 		<polygon points="6.1,15.6 0.4,5.9 3.9,5.9 6.6,10.4 14.6,1.3 18.9,1.3 6.1,15.6 	"/>
-	// 	</g>
-	// </svg>
-	// const { registerPlugin } = wp.plugins;
-	// registerPlugin('redux-full-template-export', {
-	// 	icon: redux_templatesIcon,
-	// 	render: ExportContentMenuItem,
-	// });
+  registerPlugin('redux-full-template-export', {
+     icon: redux_templatesIcon,
+     render: ExportContentMenu,
+  });
 }
