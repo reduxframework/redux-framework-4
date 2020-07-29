1854bc798e24cbfada2be4f11dc07866492e4b34
Dovy Paukstys <me@dovy.io>
2020-07-29 17:38:55 -0400

    Start of test framework using wp-browser

---
+++ tests/unit/redux-core/inc/fields/color/CSSOutputTest.php
@@ -0,0 +1,20 @@
+<?php
+// file tests/unit/EmailValidatorTest.php
+use Redux_Core;
+
+class EmailValidatorTest extends Codeception\Test\Test {
+	public function test_good_email_validation() {
+		$core = Redux_Core::instance();
+		$field_class = 'Redux_Color';
+		$field_object = new $field_class( array(), array(), $core );
+
+		if ( method_exists( $field_class, 'css_style' ) ) {
+			$style_data = $field_object->css_style( $field_object->value );
+		}
+
+
+		$this->assertTrue( $color->validate( 'not-an-email' ) );
+
+	}​
+
+}
