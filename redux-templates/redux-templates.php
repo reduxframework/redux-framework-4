01756307c898de654746fa0b458fcf1cbb93c193
Dovy Paukstys <me@dovy.io>
2020-07-28 21:25:55 -0400

    Maybe fix it?

--- redux-templates/redux-templates.php
+++ redux-templates/redux-templates.php
@@ -35,6 +35,7 @@ define( 'REDUXTEMPLATES_DIR_PATH', trailingslashit( plugin_dir_path( __FILE__ ) 
 
 // Version Check & Include Core.
 if ( version_compare( PHP_VERSION, '5.4', '>=' ) && version_compare( get_bloginfo( 'version' ), '5.4', '>=' ) ) {
+	$core = Redux_Core::instance();
 	Redux_Functions_Ex::register_class_path( 'ReduxTemplates', REDUXTEMPLATES_DIR_PATH . 'classes/' );
 	new ReduxTemplates\Init();
 }
