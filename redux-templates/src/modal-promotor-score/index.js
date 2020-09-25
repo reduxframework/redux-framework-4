874596f480e36a843ff9ffdd4b1f40d6a9240eac
Dovy Paukstys <me@dovy.io>
2020-09-25 08:57:09 -0400

    Fixes for NPS

--- redux-templates/src/modal-promotor-score/index.js
+++ redux-templates/src/modal-promotor-score/index.js
@@ -6,7 +6,8 @@ const { createSuccessNotice, createErrorNotice } = dispatch('core/notices');
 
 import { Button, ButtonGroup } from '@wordpress/components';
 
-import '../modals.scss'
+import '../modals.scss';
+import './style.scss';
 
 export default function PromotorScoreModal(props) {
     const {propOnClose} = props; // from parent
@@ -15,13 +16,14 @@ export default function PromotorScoreModal(props) {
 
     const afterPost = (response) => {
         console.log(response);
+	    propOnClose();
         if (response.data && response.data.success) {
             createSuccessNotice(__('Thanks for your feedback, your input is very much valued.'), { type: 'snackbar' });
         } else {
             createErrorNotice(response.data.message || __('Error'), { type: 'snackbar' });
         }
         delete redux_templates.nps;
-        propOnClose();
+
     }
 
     const onCloseWizard = () => {
@@ -37,25 +39,26 @@ export default function PromotorScoreModal(props) {
         <div className="redux-templates-modal-overlay">
             <div className="redux-templates-modal-wrapper">
                 <div className="redux-templates-modal-header">
-                    <h3>{__('How do you like it?', redux_templates.i18n)}</h3>
+                    <h3>{__('Can we ask you a question?', redux_templates.i18n)}</h3>
                     <button className="redux-templates-modal-close" onClick={onCloseWizard}>
                         <i className={'fas fa-times'}/>
                     </button>
                 </div>
                 <div className="redux-templates-psmodal-content">
+	                <h3>{redux_templates.nps}</h3>
                     <ButtonGroup>
                         {
-                            [...Array(10).keys()].map((i) => <Button isPrimary={score === i} onClick={()=>setScore(i)}>{ i + 1 }</Button>)
+                            [...Array(10).keys()].map((i) => <Button key={i} isPrimary={score === i} onClick={()=>setScore(i)}>{ i + 1 }</Button>)
                         }
                     </ButtonGroup>
                 </div>
                 <div className="redux-templates-modal-footer nps-footer">
-                    <button className="button button-primary" onClick={() => submitScore()}>
-                        {__('Submit Score', redux_templates.i18n)}
+                    <button className="button button-primary" disabled={!score} onClick={() => submitScore()}>
+                        {__('Submit', redux_templates.i18n)}
                     </button>
-                    <button className="button button-secondary" onClick={onCloseWizard}>
-                        {__('Cancel', redux_templates.i18n)}
-                    </button>
+                    <a href="#" onClick={onCloseWizard}>
+                        {__('Close', redux_templates.i18n)}
+                    </a>
                 </div>
             </div>
         </div>
